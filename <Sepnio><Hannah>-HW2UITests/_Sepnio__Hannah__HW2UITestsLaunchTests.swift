70fb46e2eb36f559d5f7af3a18ca70ef6ad12e11
hannahrvs <124491930+hannahrvs@users.noreply.github.com>
2023-09-14 10:58:25 -0500

    Add files via upload

---
+++ <Sepnio><Hannah>-HW2UITests/_Sepnio__Hannah__HW2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _Sepnio__Hannah__HW2UITestsLaunchTests.swift
+//  <Sepnio><Hannah>-HW2UITests
+//
+//  Created by Hannah Sepnio on 9/10/23.
+//
+
+import XCTest
+
+final class _Sepnio__Hannah__HW2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
